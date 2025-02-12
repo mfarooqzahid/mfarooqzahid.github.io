851c2df85f9aa12af9f8a4f9eba455b2c5d55d24
Farooq Zahid <farooqinfocus@gmail.com>
2025-02-12 19:51:30 +0500

    Home Page

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],  // Ensure Tailwind scans your files
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
